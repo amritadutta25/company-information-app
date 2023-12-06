02a9ee36a40ca2a23c1a21c2d9180fa1d7861c0d
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-12-05 22:24:58 -0500

    Added ABOUT section, logo and the header

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const apikey = "AAYEBOO4YJ3BWSH"
 const baseURL = "https://www.alphavantage.co/query"
 let url;
 
+const aboutCompany = $(".about")  // company 'About' section
+
 
 
 //*********************** */
@@ -12,23 +14,66 @@ let url;
 //************************ */
 // function to fetch the data
 
-function getCompanyInfo(ticker, infoType) {
+function getCompanyInfo(ticker) {
 
 
-    if (infoType === "Overview") {
-        console.log('here')
-        url = `${baseURL}?function=${infoType}&symbol=${ticker}&apikey=${apikey}`
-    }
-    else if (infoType === 'TIME_SERIES_DAILY') {
-        // compact gives last 100 days of data
-        url = `${baseURL}?function=${infoType}&symbol=${ticker}&outputsize=compact&apikey=${apikey}`
-    }
+    // if (infoType === "Overview") {
+    //     console.log('here')
+    //     url = `${baseURL}?function=${infoType}&symbol=${ticker}&apikey=${apikey}`
+    // }
+    // else if (infoType === 'TIME_SERIES_DAILY') {
+    //     // compact gives last 100 days of data
+    //     url = `${baseURL}?function=${infoType}&symbol=${ticker}&outputsize=compact&apikey=${apikey}`
+    // }
+
+    url = `${baseURL}?function=Overview&symbol=${ticker}&apikey=${apikey}`
 
     //fetch from the url
     fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+        renderAboutCompany(data)
+    })
+
+}
+
+// function that render 'About' section
+function renderAboutCompany(data){
+
+    // alter the HTML inside the div.about
+    aboutCompany.append(
+        `<h2>About</h2>
+        <p>I${data.Description}</p>
+        <div class="address">${data.Address}</div>
+        <div class="sector">${data.Sector}</div>
+        <div class="industry">${data.Industry}</div>
+        `
+    )
     
+}
+
+//function to handle the form submission
+function handleSubmit(event){
+    
+    // prevent the refreshing of the page from the form submission
+    // refreshing of the page is the default behaviour of forms
+    event.preventDefault()
+
+    //grab the form from the event
+    const form = event.target
+    
+    //create a FormData object to access the form data
+    const formData = new FormData(form)
+    //grab the movieTitle
+    const ticker = formData.get("ticker") //name='ticker"
+    
+    //fetch the specified ticker
+    getCompanyInfo(ticker)
+
+    // clear the input form
+    const $input = $("input")
+    $input.val("")
+
 }
 
 
@@ -37,4 +82,4 @@ function getCompanyInfo(ticker, infoType) {
 //************************ */
 
 //initial call to populate the first ticker
-getCompanyInfo("IBM",'Overview')
+getCompanyInfo("BLK")
